9c010499d62b62fbad9cfba1ffaceb2eb4e26a7d
Unknown <D227502@edu.rocwb.nl>
2018-04-12 21:28:59 +0200

    Added code for website checker

---
+++ IsThisWebsiteOnline/IsThisWebsiteOnline/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net;
+
+
+namespace IsThisWebsiteOnline
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ipaddress = textBox1.Text;
+
+            IPAddress[] addresslist = Dns.GetHostAddresses(ipaddress);
+
+            foreach (IPAddress theaddress in addresslist)
+            {
+
+                string lel = theaddress.ToString();
+
+                Ping pingSender = new Ping();
+                IPAddress address = IPAddress.Parse(lel);
+                PingReply reply = pingSender.Send(address);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    label1.Text = "WebAddress: " + ipaddress;
+                    label2.Text = "Address: " + reply.Address.ToString();
+                    label3.Text = "Status: " + reply.Status.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("No connection");
+                }
+            }
+        }
+
+    }
+}
